941375d34c04c269e9e1ea3a8f355f20c12a352b
Sebastián García <johancs.mm@gmail.com>
2024-04-30 14:04:52 -0500

    Add deno adapter for deploy (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import astroI18next from 'astro-i18next'
+import deno from '@deno/astro-adapter'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react(), astroI18next()],
+  output: 'hybrid',
+  adapter: deno(),
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react(),
+    astroI18next(),
+    sitemap(),
+  ],
 })
