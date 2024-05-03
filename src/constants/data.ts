cf5fafc20fba130d5b3035a02857821a33babaaa
Sebastián García <johancs.mm@gmail.com>
2024-05-03 11:04:11 -0500

    Add page testimonials (#12)

---
+++ src/constants/data.ts
@@ -0,0 +1,32 @@
+export const scientists = [
+  {
+    name: 'Dune',
+    author: 'Frank Herbert',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+  {
+    name: 'Foundation',
+    author: 'Isaac Asimov',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+  {
+    name: '1984',
+    author: 'George Orwell',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+  {
+    name: 'Brave New World',
+    author: 'Aldous Huxley',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+  {
+    name: 'The Hitchhiker\'s Guide to the Galaxy',
+    author: 'Douglas Adams',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+  {
+    name: 'The Hitchhiker\'s Guide to the Galaxy',
+    author: 'Douglas Adams',
+    description: 'NutriPlan has revolutionized my meal planning and nutrition journey. With its intuitive Notion template, I can effortlessly plan my meals, track my nutrition,and stay on top of my health goals. It"s truly a game-changer!',
+  },
+]
